63eb828b220cebd349dff31dff10140cead5acc3
Qaidy <qaidyidn@gmail.com>
2026-07-15 10:38:54 +0700

    feat: add Dart exercises for boolean and operator types

---
+++ 8_boolean.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  var isTrue = true;
+  var isFalse = false;
+
+  print(isTrue);
+  print(!isFalse);
+}
---
+++ 9_operator.dart
@@ -0,0 +1,30 @@
+void main(List<String> args) {
+  // operator aritmatika
+  var a = 5;
+  var b = 2;
+
+  print(a + b);
+  print(a - b);
+  print(a * b);
+  print(a / b);
+  print(a % b);
+
+  // operator relasi perbandingan
+  print(a > b);
+  print(a < b);
+  print(a >= b);
+  print(a <= b);
+  print(a == b);
+  print(a != b);
+
+  // operator increment dan decrement
+  print(a++);
+  print(a--);
+  print(++a);
+  print(--a);
+
+  // operator logika
+  print(a > b && a < b);
+  print(a > b || a < b);
+  print(!(a > b));
+}
